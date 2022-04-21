aa84dd22624677a621f7c76d5f7115de6e47429b
Amira <amira.elnashar13516@gmail.com>
2022-04-21 14:20:12 +0400

    flask stuff

--- app.py
+++ app.py
@@ -22,5 +22,27 @@ mysql = MySQL(app)
 @app.route('/', methods=['GET', 'POST'])
 def login():
     # Output message if something goes wrong...
-    msg = 'something went wrong'
-    return render_template('Home.html', msg='')
+    msg = ''
+    # Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'txtEmail' in request.form and 'txtPassword' in request.form:
+        # Create variables for easy access
+        email = request.form['txtEmail']
+        password = request.form['password']
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM users WHERE email = %s AND password = %s', (email, password,))
+        # Fetch one record and return result
+        account = cursor.fetchone()
+        # If account exists in accounts table in out database
+        if account:
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['user_id'] = account['user_id']
+            session['username'] = account['username']
+            # Redirect to home page
+            return render_template('Home.html')
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect username/password!'
+    # Show the login form with message (if any)
+    return render_template('login.html', msg=msg)
