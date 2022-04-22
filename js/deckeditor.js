a246ef96a738561291a6355eb8a24f86f7d295ac
Mikaela Eunice Magsumbol <mika.mags@yahoo.com>
2022-04-23 02:03:15 +0400

    wkring on editdeck

---
+++ js/deckeditor.js
@@ -0,0 +1,6 @@
+const flashcard = document.getElementsByClassName("flashcards")[0];
+const createBox = document.getElementsByClassName("create-box")[0];
+const question = document.getElementById("question");
+const answer = document.getElementById("answer");
+
+// let contentArray = 
