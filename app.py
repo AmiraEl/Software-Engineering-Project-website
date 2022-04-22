9e6c1abeeeb453c96887480ec63395648d754136
Amira <amira.elnashar13516@gmail.com>
2022-04-22 15:02:42 +0400

    app.py

--- app.py
+++ app.py
@@ -17,13 +17,17 @@ app.config['MYSQL_DB'] = 'swe'
 # Intialize MySQL
 mysql = MySQL(app)
 
-
 # http://localhost:5000/pythonlogin/ - the following will be our login page, which will use both GET and POST requests
 @app.route('/', methods=['GET', 'POST'])
+def landing():
+    return render_template("index.html")
+
+
+
+# http://localhost:5000/pythonlogin/ - the following will be our login page, which will use both GET and POST requests
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    # Output message if something goes wrong...
-    msg = ''
-    # Check if "username" and "password" POST requests exist (user submitted form)
+       # Check if "username" and "password" POST requests exist (user submitted form)
     if request.method == 'POST' and 'txtEmail' in request.form and 'txtPassword' in request.form:
         # Create variables for easy access
         email = request.form['txtEmail']
@@ -33,6 +37,7 @@ def login():
         cursor.execute('SELECT * FROM users WHERE email = %s AND password = %s', (email, password,))
         # Fetch one record and return result
         account = cursor.fetchone()
+        print(account)
         # If account exists in accounts table in out database
         if account:
             # Create session data, we can access this data in other routes
@@ -41,8 +46,6 @@ def login():
             session['username'] = account['username']
             # Redirect to home page
             return render_template('Home.html')
-        else:
-            # Account doesnt exist or username/password incorrect
-            msg = 'Incorrect username/password!'
+
     # Show the login form with message (if any)
-    return render_template('login.html', msg=msg)
+    return render_template('login.html')
